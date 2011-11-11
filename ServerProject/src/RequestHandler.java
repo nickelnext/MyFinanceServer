93502cb886928ba1759ec415f81fb5a27fb1d3ee
franco.maroni@gmail.com <franco.maroni@gmail.com@97024e3e-a4d6-edea-a7a6-9c322da05964>
2011-11-11 17:52:45 +0000

    solo RequestHandler.java
    git-svn-id: http://svn.codespot.com/a/eclipselabs.org/mangano-marconi-server/trunk@41 97024e3e-a4d6-edea-a7a6-9c322da05964

--- ServerProject/src/RequestHandler.java
+++ ServerProject/src/RequestHandler.java
@@ -4,11 +4,14 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Vector;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
 import Handlers.SiteInterface;
 import Quotes.Quotation;
 import Quotes.Type;
 import Requests.Request;
 import Requests.RequestContainer;
+import Search.Search;
 
 public class RequestHandler {
 
@@ -20,25 +23,27 @@ public class RequestHandler {
 	 * @throws MalformedURLException 
 	 */
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, MalformedURLException {
-//		Request r = new Request("IT0003844534", Type.BTP);
+
+		//TEST:
+		//creo una lista di richieste  e la faccio processare dall'handler
+		//poi stampo il contenuto delle quotation ritornate
 		ArrayList<Request> tmpList = new ArrayList<Request>();
-		tmpList.add(new Request("IT0003844534", Type.BTP));
-		tmpList.add(new Request("IT0004615917", Type.BTP));
-		tmpList.add(new Request("IT0003618383", Type.BTP));
-		tmpList.add(new Request("IT0004356843", Type.BTP));
+		tmpList.add(new Request("IT0004572910", Type.BTP));
+		//tmpList.add(new Request("IT0004356843", Type.BTP));
 		RequestContainer cont = new RequestContainer();
 		cont.setReqList(tmpList);
 		ArrayList<Quotation> resList = processRequests(cont.getReqList());
-		
-		for (int j = 0; j < resList.size(); j++) {	
-			System.out.println(resList.get(j).toString());
+		if(null != resList){
+			for (int j = 0; j < resList.size(); j++) {	
+				System.out.println(resList.get(j).toString());
+			}
 		}
 	}		
 		
 	
 	public static ArrayList<Quotation> processRequests(ArrayList<Request> arrReq)throws InstantiationException, IllegalAccessException, ClassNotFoundException, MalformedURLException {
 
-		MyDatabase db = new MyDatabase("pinella", "pinella", "aa8bbf9b");
+		MyDatabase db = new MyDatabase("pinella", "pinella", "pinella87");
 		// connessione al db
 		if ( !db.connect() ) { 
 			System.out.println("Errore durante la connessione.");
@@ -46,17 +51,97 @@ public class RequestHandler {
 			System.exit(0);
 		}
 
+		Vector rating = db.execQuery("SELECT Name,SearchPath FROM tbl_name_search_rate ORDER BY HitRate DESC;");
+		if(0 != rating.size()){
+			System.out.println("TABELLA NAME_SEARCH VUOTA");
+		}
+		
 		ArrayList<Quotation> quotList = new ArrayList<Quotation>();
+		Search isinFinder;
 		
 		for (int countRequests = 0; countRequests < arrReq.size(); countRequests++){	
 
-			String requestedSite;
-			String requestedUrl;
+			String requestedSite = null; //provider estratto dalla prima query
+			String requestedSearchUrl = null; //url di ricerca estratta dalla prima query
+			String requestedDetailsUrl = null; //url contenente i dettagli del titolo trovata dalla search
 			boolean found;
 			Quotation quot = null;
-
+			String[] nameSearchPath;
+			String genericUrl = null;
+			       
 			Request req = arrReq.get(countRequests);
+			found = false;
+			for (int countSites = 0; countSites < rating.size() && found == false; countSites++) {
+				nameSearchPath = (String[]) rating.elementAt(countSites);
+				requestedSite = nameSearchPath[0];
+				requestedSearchUrl = nameSearchPath[1];
+				System.out.println("istanzio una classe "+requestedSite+"Search ..."+" e lancio il parser per il sito "+requestedSearchUrl);				
+				isinFinder = (Search) Class.forName("Search."+requestedSite+"_Search").newInstance();
+				
+				if(isinFinder.search(req.getISIN(), requestedSearchUrl)){
+					System.out.println("ISIN trovato, vado a prendermi le info");
+					found = true;
+					genericUrl = isinFinder.getBaseLink();
+					requestedDetailsUrl = isinFinder.getCompleteLink();
+				}else{
+					System.out.println("ISIN non trovato, provo con il provider successivo");
+				}				
+			}
+			if(found){
+				
+				System.out.println("SELECT Type FROM tbl_name_type_url WHERE Name="+requestedSite+" AND Url="+genericUrl);
+//				Vector typeQuery = db.execQuery("SELECT Type FROM tbl_name_type_url WHERE Name=\""+requestedSite+"\" AND Url=\""+genericUrl+"\"");
+				Vector typeQuery = db.execQuery("SELECT Type FROM tbl_name_type_url WHERE Url=\""+genericUrl+"\"");
+//				Vector typeQuery = db.execQuery("SELECT Type FROM tbl_name_type_url WHERE Name=\""+requestedSite+"\"");
+				if(0 == typeQuery.size()){
+					System.out.println("URL NON TROVATA!!");
+					return null;
+				}
+				String[] reqType = (String[])typeQuery.elementAt(0);
+				SiteInterface detailsParser = (SiteInterface)Class.forName("Sites."+requestedSite).newInstance();
+				Type t = Type.valueOf(reqType[0]);
+				
+				switch (t) {
+				case BTP:
+					quot = detailsParser.parseBTP(new URL (requestedDetailsUrl));
+					break;
+				case BOT:
+					quot = detailsParser.parseBOT(new URL (requestedDetailsUrl));
+					break;
+				case CCT:
+					quot = detailsParser.parseCCT(new URL (requestedDetailsUrl));
+					break;
+				case CTZ:
+					quot = detailsParser.parseCTZ(new URL (requestedDetailsUrl));
+					break;
+				case BOND:
+					quot = detailsParser.parseBOND(new URL (requestedDetailsUrl));
+					break;
+				case SHARE:
+					quot = detailsParser.parseSHARE(new URL (requestedDetailsUrl));
+					break;
+				case FUND:
+					quot = detailsParser.parseFUND(new URL (requestedDetailsUrl));
+					break;
+				default:
+					quot = detailsParser.parseBTP(new URL (requestedDetailsUrl));					
+				}
+				//da aggiustare magari con un'eccezione
+				//può essere che un giorno un link che funzionava ed era 
+				//in memoria smetta di funzionare e il parsing restituisca null
+			}
+
+			if(null != quot){
+				quot.setSite(requestedSite);
+				quot.setISIN(req.getISIN());
+				quotList.add(quot);
+			}else {
+				System.out.println("Errore nel parsing! Quotazione nulla!");				
+			}
+		}
 			
+			
+/*			
 			//eseguo ricerca nella tabella isin_site
 			Vector v1 = db.execQuery("SELECT site,url FROM tbl_isin_site WHERE ISIN=\""+req.getISIN()+"\";" );
 
@@ -178,8 +263,9 @@ public class RequestHandler {
 					System.out.println("quotazione non trovata.. controllare correttezza di ISIN e tipo");
 				}
 			}
-			
-		}//end while
+	
+	}//end while
+	*/
 
 		db.disconnect();
 		

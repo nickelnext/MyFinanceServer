33e4074021a3b1992278117bfd1015d9f09e7f98
nickelnext@gmail.com <nickelnext@gmail.com@97024e3e-a4d6-edea-a7a6-9c322da05964>
2011-11-14 10:15:09 +0000

    git-svn-id: http://svn.codespot.com/a/eclipselabs.org/mangano-marconi-server/trunk@48 97024e3e-a4d6-edea-a7a6-9c322da05964

--- ServerProject/src/Search/Yahoo_Finanza_it_Search.java
+++ ServerProject/src/Search/Yahoo_Finanza_it_Search.java
@@ -43,9 +43,9 @@ public class Yahoo_Finanza_it_Search extends Search {
 			if(nodes.getLength()==0)		//No nodes, probably a 404 error
 				return false;
 			
-			this.setBaseLink("http://it.finance.yahoo.com/q?s="+UtilFuncs.ISIN_REPLACE);
-			String s = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22" + nodes.item(0).getNodeValue() + "%22%29&env=store://datatables.org/alltableswithkeys";
-			this.setCompleteLink(s);
+			String s = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22" + Utils.UtilFuncs.ISIN_REPLACE + "%22%29&env=store://datatables.org/alltableswithkeys";
+			this.setBaseLink(s);
+			this.setCompleteLink(s.replace(UtilFuncs.ISIN_REPLACE, nodes.item(0).getNodeValue()));
 			this.setCode(nodes.item(0).getNodeValue());
 			this.setISIN(ISIN);
 			

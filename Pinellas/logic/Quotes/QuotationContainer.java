2ffedd79587f6a8de25fe867f87890e09204cfbf
franco.maroni@gmail.com <franco.maroni@gmail.com@97024e3e-a4d6-edea-a7a6-9c322da05964>
2011-12-19 09:56:18 +0000

    QuotationContainer.java
    git-svn-id: http://svn.codespot.com/a/eclipselabs.org/mangano-marconi-server/trunk@70 97024e3e-a4d6-edea-a7a6-9c322da05964

---
+++ Pinellas/logic/Quotes/QuotationContainer.java
@@ -0,0 +1,51 @@
+package Quotes;
+
+import java.util.ArrayList;
+
+public class QuotationContainer {
+
+	private ArrayList<Quotation_Bond> bondList;
+	private ArrayList<Quotation_Fund> fundList;
+	private ArrayList<Quotation_Share> shareList;
+
+	
+	
+	public QuotationContainer() {
+		
+	}
+	
+	public ArrayList<Quotation_Bond> getBondList() {
+		if(null == this.bondList){
+			this.bondList = new ArrayList<Quotation_Bond>();
+		}
+		return bondList;
+	}
+	public void setBondList(ArrayList<Quotation_Bond> bondList) {
+		this.bondList = bondList;
+	}
+
+	
+	public ArrayList<Quotation_Fund> getFundList() {
+		if(null == this.fundList){
+			this.fundList = new ArrayList<Quotation_Fund>();
+		}
+		return fundList;
+	}
+	public void setFundList(ArrayList<Quotation_Fund> fundList) {
+		this.fundList = fundList;
+	}
+
+
+	public ArrayList<Quotation_Share> getShareList() {
+		if(null == this.shareList){
+			this.shareList = new ArrayList<Quotation_Share>();
+		}
+		return shareList;
+	}
+	public void setShareList(ArrayList<Quotation_Share> shareList) {
+		this.shareList = shareList;
+	}
+	
+
+
+}
